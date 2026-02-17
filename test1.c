eedd5ea36b48fe0b18e59fa7ba32d898f3a6bdd6
MananKaneriya <124285772+MananKaneriya@users.noreply.github.com>
2026-02-17 17:16:10 +0530

    Add files via upload

---
+++ test1.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+/*int main() {
+   int length ;
+   printf("Enter value of Length: ");
+   scanf("%d", &length);
+   printf("Value of length is: %d\n", length);
+   int width ;
+   printf("Enter value of width: ");
+   scanf("%d", &width);
+   printf("Value of width is: %d \n", width);
+   int area = length * width;
+   printf("Area of rectangle is: %d \n", area);
+    return 0;
+}*/
+
+/*int main() {
+   float pi = 3.14 ;
+   
+   int radius ;
+   printf("Enter value of radius: ");
+   scanf("%d", &radius);
+   printf("Value of radius is: %d \n", radius);
+
+   int height ;
+   printf("Enter value of height: ");
+   scanf("%d", &height);
+
+   float vol_circle = pi * radius * radius * height;
+   printf("Volume of circle is: %.2f \n", vol_circle);
+    return 0;
+}*/
+
+/*int main() {
+   int p ;
+   printf("Enter value of Principle: ");
+   scanf("%d", &p);
+
+   int i ;
+   printf("Enter value of number of Intrest: ");
+   scanf("%d", &i);
+
+   int r ;
+   printf("Enter value of Rate of intrest: ");
+   scanf("%d", &r);
+
+   float SI = (p * i * r) / 100;
+   printf("Simple Interest is: %.2f \n", SI);
+    return 0;
+}*/
+/*int main() {
+   float k = 3.0/9;
+   printf("Value of k is: %f \n", k); 
+   
+    return 0;
+}*/
+
+/*int main() {
+   char dt = '16 feb 2026';
+   printf("character is: %c \n", dt); 
+    u cant put string in char data type, it will only take first character of the string and ignore the rest.
+    return 0;
+}*/
+
+/*int main() {
+   int a = 3349895;
+   printf("Value of a%97 is: %d \n", a%97); 
+   checking numbe ris divisible by 97 or not, if the result is 0 then it is divisible otherwise it is not.
+    return 0;
+}*/
+int main() {
+   int x = 2;
+   int y = 3;
+   int z = 3;
+   int k = 1;
+   
+   printf("Answere of expression: %d \n", 3*x/y-z+k); 
+   
+    return 0;
+}
